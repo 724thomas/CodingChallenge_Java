a5968cf0c6b2da9bee731a95be778c109cf8045a
WonJoonChoi <724thomas@gmail.com>
2022-11-06 01:05:07 +0900

    221106

--- src/kr/co/programmers/school/k의개수.java
+++ src/kr/co/programmers/school/k의개수.java
@@ -1,2 +1,18 @@
-package kr.co.programmers.school;public class k의개수 {
+package kr.co.programmers.school;
+//k의 개수
+//https://school.programmers.co.kr/learn/courses/30/lessons/120887
+public class k의개수 {
+    public int solution(int i, int j, int k) {
+        int count=0;
+        String temp="";
+        for(int a=i; a<j+1; a++){
+            temp+=String.valueOf(a);
+        }
+        for(int a=0; a<temp.length(); a++){
+            if((Integer.parseInt(temp.charAt(a)+""))==k){
+                count++;
+            }
+        }
+        return count;
+    }
 }
