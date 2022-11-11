557d9ea0ed66af86850298dac18a051a9be4408c
WonJoonChoi <724thomas@gmail.com>
2022-11-11 23:43:23 +0900

    221111

---
+++ src/kr/co/programmers/school/A로_B만들기.java
@@ -0,0 +1,21 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120886
+package kr.co.programmers.school;
+
+public class A로_B만들기 {
+    public int solution(String before, String after) {
+        if (sortString(before).equals(sortString(after))){
+            return 1;
+        }
+        return 0;
+    }
+
+    public String sortString(String str) {
+        str = str.chars()        // IntStream
+                .sorted()
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+        return str;
+    }
+}
