80c32b3a1d9d788db50759df091957da8460bc58
724thomas <724thomas@gmail.com>
2024-09-10 00:21:50 +0900

    240910

---
+++ src/Leetcode/Q215.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Q215 {
+    public int findKthLargest(int[] nums, int k) {
+        int cmax = -Integer.MAX_VALUE;
+        int cmin = Integer.MAX_VALUE;
+        for (int num:nums){
+            cmax = Math.max(cmax, num);
+            cmin = Math.min(cmin, num);
+        }
+
+        int[] counter = new int[cmax-cmin+1];
+        for (int num:nums){
+            counter[num-cmin]++;
+        }
+
+        for (int i=counter.length-1; i>=0; i--){
+            if (k > counter[i]) {
+                k-=counter[i];
+            } else {
+                return i + cmin;
+            }
+        }
+        return 1;
+    }
+}
