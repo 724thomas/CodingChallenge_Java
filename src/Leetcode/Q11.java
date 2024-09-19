fa20fa7e433b4da1b8a82e3342db983371472a07
724thomas <724thomas@gmail.com>
2024-09-20 02:38:51 +0900

    240923

--- src/Leetcode/Q11.java
+++ src/Leetcode/Q11.java
@@ -1,6 +1,9 @@
 package Leetcode;
 
-public class Q11 {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class  Q11 {
     class Solution {
         public int maxArea(int[] height) {
             var n = height.length;
@@ -21,5 +24,23 @@ public class Q11 {
             }
             return ans;
         }
+    }
+}
+
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        var n = genres.length;
+        var genreMap = new HashMap<String, ArrayList<int[]>>();
+        for (int i=0; i<n; i++) {
+            var genre = genres[i];
+            var play = plays[i];
+            genreMap.putIfAbsent(genre, new ArrayList<>());
+            genreMap.get(genre).add(new int[]{play, i});
+        }
+
+
+        int[] answer = {};
+        return answer;
     }
 }
