93dce05039a7a8a7b70606f3b78b6c41af85f0d8
724thomas <724thomas@gmail.com>
2024-09-25 08:32:08 +0900

    240925

---
+++ src/Leetcode/Q990.java
@@ -0,0 +1,62 @@
+package Leetcode;
+
+public class Q990 {
+    class Solution {
+        int[] par;
+        int[] rank;
+
+        public int find(int x) {
+            if (x != par[x]) {
+                par[x] = find(par[x]);
+            }
+            return par[x];
+        }
+
+        public void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+
+            if (rootA != rootB) {
+                if (rank[rootA] > rank[rootB]) {
+                    par[rootB] = rootA;
+                } else if (rank[rootA] < rank[rootB]) {
+                    par[rootA] = rootB;
+                } else {
+                    par[rootB] = rootA;
+                    rank[rootA]++;
+                }
+            }
+        }
+
+        public boolean equationsPossible(String[] equations) {
+            par = new int[26];
+            rank = new int[26];
+
+            for (int i = 0; i < 26; i++) {
+                par[i] = i;
+                rank[i] = 0;
+            }
+
+            for (String equation : equations) {
+                if (equation.charAt(1) == '=') {
+                    int a = equation.charAt(0) - 'a';
+                    int b = equation.charAt(3) - 'a';
+                    union(a, b);
+                }
+            }
+
+            for (String equation : equations) {
+                if (equation.charAt(1) == '!') {
+                    int a = equation.charAt(0) - 'a';
+                    int b = equation.charAt(3) - 'a';
+                    if (find(a) == find(b)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
