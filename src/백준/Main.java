15f0456defa776dd32c9125ba0544c31d4e7e656
724thomas <724thomas@gmail.com>
2024-10-12 08:29:03 +0900

    241012

--- src/백준/Main.java
+++ src/백준/Main.java
@@ -7,12 +7,15 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 class Main {
+    static String input;
+    static String[] inputs;
+    static int[] nimputs;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//        String input = br.readLine();
-//        String[] inputs = br.readLine().split(" ");
-//        int[] numinputs = Arrays.stream(br.readLine().split(" "))
+//        input = br.readLine();
+//        inputs = br.readLine().split(" ");
+//        numinputs = Arrays.stream(br.readLine().split(" "))
 //                .mapToInt(Integer::parseInt)
 //                .toArray();
 
