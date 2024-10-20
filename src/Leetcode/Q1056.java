5c717fcbf684da1ba1273884e9d284f0e8c8093b
724thomas <724thomas@gmail.com>
2024-10-20 18:00:17 +0900

    241020

---
+++ src/Leetcode/Q1056.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1056 {
+    class Solution {
+        public boolean confusingNumber(int n) {
+            Map<Character, Character> numMap = new HashMap<>();
+            numMap.put('0','0');
+            numMap.put('1','1');
+            numMap.put('6','9');
+            numMap.put('8','8');
+            numMap.put('9','6');
+            String ns = String.valueOf(n);
+            StringBuilder sb = new StringBuilder();
+            for (int i=ns.length()-1; i>-1; i--){
+                char target = ns.charAt(i);
+                if (!numMap.containsKey(target)) return false;
+                sb.append(numMap.get(target));
+            }
+
+            return !sb.toString().equals(ns);
+        }
+    }
+}
