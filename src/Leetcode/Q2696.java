72046ca890c3d7f9a937865de02b18d822c23087
724thomas <724thomas@gmail.com>
2024-11-07 17:58:50 +0900

    241107

---
+++ src/Leetcode/Q2696.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class Q2696 {
+    class Solution {
+        public int minLength(String s) {
+            while (s.contains("AB") || s.contains("CD")) s= s.replace("AB","").replace("CD","");
+            return s.length();
+        }
+    }
+}
