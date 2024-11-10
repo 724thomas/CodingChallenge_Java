165a62ede4bf6025e62ca23f724c45adb565063b
724thomas <724thomas@gmail.com>
2024-11-11 04:58:07 +0900

    241111

---
+++ src/Leetcode/Q252.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+public class Q252 {
+    class Solution {
+        public boolean canAttendMeetings(int[][] intervals) {
+            Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+            int start = -1;
+            for (int i=0; i<intervals.length; i++) {
+                if (start > intervals[i][0]) return false;
+                start = intervals[i][1];
+            }
+            return true;
+        }
+    }
+}
