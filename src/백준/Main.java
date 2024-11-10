d752eda3a85edac4845d411bbdb3808f62793f5e
724thomas <724thomas@gmail.com>
2024-11-10 23:01:24 +0900

    241110

--- src/백준/Main.java
+++ src/백준/Main.java
@@ -1,6 +1,17 @@
 // https://www.acmicpc.net/problem/
 package 백준;
 
+/*
+
+
+1. 아이디어 :
+
+2. 시간복잡도 :
+O( )
+3. 자료구조 :
+
+
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
