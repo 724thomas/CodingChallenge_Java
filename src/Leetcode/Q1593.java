cd1bebfd7c760cbcbb0980aeb287c3ab740ae7c8
724thomas <724thomas@gmail.com>
2024-11-11 23:47:53 +0900

    241111

---
+++ src/Leetcode/Q1593.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q1593 {
+    class Solution {
+        public int dfs(Set<String> visited, String s, int idx, int count) {
+            if (idx >= s.length()) return count;
+            StringBuilder substring = new StringBuilder();
+            int cmax = 0;
+            for (int i = idx; i<s.length(); i++) {
+                substring.append(s.charAt(i));
+                if (visited.contains(substring.toString())) continue;
+
+                visited.add(substring.toString());
+
+                cmax = Math.max(cmax, dfs(visited, s, i+1, count+1));
+
+                visited.remove(substring.toString());
+            }
+
+            return cmax;
+        }
+
+        public int maxUniqueSplit(String s) {
+            return dfs(new HashSet<>(), s, 0, 0);
+        }
+    }
+}
