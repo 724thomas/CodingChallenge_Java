0ae6d8b4bd3f03032d293ced48410ec8d65bf060
724thomas <724thomas@gmail.com>
2024-11-19 10:17:53 +0900

    241119

--- src/프로그래머스/소수 찾기2.py
+++ src/프로그래머스/소수 찾기2.py
@@ -7,31 +7,56 @@
     O(
 3. 자료구조 :
 
+import java.util.*;
+class Solution {
+    public Set<Integer> permutations(int[] nums) {
+        Set<Integer> ans = new HashSet<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(nums, "", ans, visited);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, String curr, Set<Integer> ans, boolean[] visited) {
+        if (curr!="") {
+            ans.add(Integer.parseInt(curr));
+        }
+        if (curr.length() == nums.length) {
+            return;
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+
+            backtrack(nums, curr + String.valueOf(nums[i]), ans, visited);
+
+            visited[i] = false;
+        }
+    }
+
+    public boolean check_prime(int n) {
+        if (n < 2) return false;
+        int sqrt = (int)Math.sqrt(n);
+
+        for (int i=2; i<=sqrt; i++) {
+            if (n%i==0) return false;
+        }
+        return true;
+    }
+
+    public int solution(String numbers) {
+        int ans = 0;
+        int[] nums = new int[numbers.length()];
+        for (int i=0; i<numbers.length(); i++) {
+            nums[i] = Integer.parseInt(numbers.charAt(i) + "");
+        }
+        for (int num : permutations(nums)) {
+            ans += check_prime(num) ? 1 : 0;
+        }
+        return ans;
+    }
+}
 '''
 
-
-import sys
-#sys.setrecursionlimit(1000000)
-input = sys.stdin.readline
-
-def solution():
-
-    return 1
-
-# n = int(input().rstrip())
-#
-# n, m = list(map(int, input().split()))
-# a = [c for c in input().strip()]
-#
-# s = input().rstrip()
-
-# arr = list(map(int, input().strip().split()))
-# integer_list = [int(num) for num in input().split()]
-# dp = [[0 for _ in range(n)] for _ in range(n)]
-# dp = [[0 for j in range(n)] for i in range(n)]
-# grid = [list(input().rstrip()) for _ in range(n)] # "aaa" "bbb"
-# grid = list(list(map(int, input().split())) for _ in range(n)) # 0 0 0 0", "0 0 0 0
-
-print(solution())
 
 
