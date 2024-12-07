b6200ae56ab2c71ab023aa8a0b95ac61beccb451
724thomas <724thomas@gmail.com>
2024-12-07 21:49:59 +0900

    241207

---
+++ src/백준/Q1254.java
@@ -0,0 +1,48 @@
+// https://www.acmicpc.net/problem/
+package 백준;
+
+/*
+
+
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O( )
+3. 자료구조 :
+
+
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1254 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int minLength = s.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s.substring(i))) {
+                minLength += i;
+                break;
+            }
+        }
+
+        System.out.println(minLength);
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
