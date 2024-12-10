be8b400eb49e9d03a8c66e3e9bb0287383c4a961
724thomas <724thomas@gmail.com>
2024-12-11 02:35:14 +0900

    241211

---
+++ src/Leetcode/Q2415.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+public class Q2415 {
+/*
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        boolean odd = false;
+        List<TreeNode> currList = new ArrayList<>();
+        currList.add(root);
+
+        while (!currList.isEmpty()) {
+            List<TreeNode> nextList = new ArrayList<>();
+            List<Integer> values = new ArrayList<>();
+            for (TreeNode node : currList) {
+                if (node.left!=null) {
+                    nextList.add(node.left);
+                    nextList.add(node.right);
+                    values.add(node.left.val);
+                    values.add(node.right.val);
+                }
+            }
+            odd = !odd;
+            if (odd) {
+                for (int i=0; i<nextList.size(); i++) {
+                    nextList.get(i).val = values.get(values.size()-1-i);
+                }
+            }
+            currList = nextList;
+        }
+        return root;
+    }
+}
+ */
+}
