ef1c01a5721f6b61e7eb495e69e9fd8f36e6a40d
724thomas <724thomas@gmail.com>
2024-12-26 02:34:42 +0900

    241226

---
+++ src/Leetcode/Q3354.java
@@ -0,0 +1,29 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q3354 {
+    class Solution {
+        public List<Integer> getInitialCurr (int[] nums) {
+            List<Integer> currs = new ArrayList<>();
+            for (int i = 0; i<nums.length; i++) if (nums[i] == 0) currs.add(i);
+            return currs;
+        }
+
+        public int countValidSelections(int[] nums) {
+            int N = nums.length, ans = 0;
+            int[] ls = new int[N], rs = new int[N];
+
+            for (int i=1; i<N; i++) {
+                ls[i] = ls[i-1] + nums[i-1];
+                rs[N-i-1] = rs[N-i] + nums[N-i];
+            }
+
+            for (int curr : getInitialCurr(nums)) {
+                ans += Math.max(0, 2 - Math.abs(ls[curr] - rs[curr]));
+            }
+            return ans;
+        }
+    }
+}
