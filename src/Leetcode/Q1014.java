1b84d7985e4cf6462c272ccc9937626e66042077
724thomas <724thomas@gmail.com>
2024-12-28 01:18:48 +0900

    241228

---
+++ src/Leetcode/Q1014.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class Q1014 {
+    class Solution {
+        public int maxScoreSightseeingPair(int[] values) {
+            int ans = 0, imax = 0;
+            for (int right=1; right<values.length; right++) {
+                ans = Math.max(ans, values[imax] + values[right] + imax - right);
+                if (values[imax] +imax  < values[right] + right) imax = right;
+            }
+            return ans;
+        }
+    }
+}
