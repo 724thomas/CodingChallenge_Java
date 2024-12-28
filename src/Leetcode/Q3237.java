0d50e447dea2cf9ae861e5cbce8bf5646f442d7a
724thomas <724thomas@gmail.com>
2024-12-29 01:33:24 +0900

    241229

---
+++ src/Leetcode/Q3237.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Q3237 {
+    class Solution {
+        public int[] simulationResult(int[] windows, int[] queries) {
+            int[] ans = new int[windows.length];
+            boolean[] visited = new boolean[windows.length+1];
+
+            int idx = 0;
+            for (int i = queries.length-1; i>-1; i--) {
+                int q = queries[i];
+                if (visited[q]) continue;
+                visited[q] = true;
+                ans[idx++] = q;
+            }
+            for (int i=0; i<windows.length; i++) {
+                int w = windows[i];
+                if (visited[w]) continue;
+                visited[w] = true;
+                ans[idx++] = w;
+            }
+
+            return ans;
+        }
+    }
+}
