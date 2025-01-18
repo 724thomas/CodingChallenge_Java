e030b4123792b20483c41e5d1a5bcc46a6612304
724thomas <724thomas@gmail.com>
2025-01-18 21:24:34 +0900

    250118

---
+++ src/Leetcode/Q1228.java
@@ -0,0 +1,16 @@
+package Leetcode;
+
+public class Q1228 {
+    class Solution {
+        public int missingNumber(int[] arr) {
+            int actualDiff = arr[1] - arr[0];
+            for (int i = 2; i < arr.length; i++) {
+                int diff = arr[i] - arr[i-1];
+                if (diff == actualDiff) continue;
+                else return arr[i] - actualDiff;
+            }
+
+            return arr[0];
+        }
+    }
+}
