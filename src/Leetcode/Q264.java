6a7e08a1a963dd127a930acb82d1b3ada4b721ff
724thomas <724thomas@gmail.com>
2025-01-19 21:26:07 +0900

    250119

---
+++ src/Leetcode/Q264.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+import java.util.TreeSet;
+
+public class Q264 {
+    class Solution {
+        public int nthUglyNumber(int n) {
+            TreeSet<Long> treeSet = new TreeSet<>();
+            treeSet.add(1L);
+            for (int i=0; i<n-1; i++) {
+                long c = treeSet.pollFirst();
+                treeSet.add(c*2);
+                treeSet.add(c*3);
+                treeSet.add(c*5);
+            }
+
+            long ans = treeSet.pollFirst();
+            return (int)ans;
+        }
+    }
+}
