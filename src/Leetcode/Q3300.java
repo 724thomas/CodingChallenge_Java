b6363ef9122ee5b6a4bc37f4f6bc2dab9159081a
724thomas <724thomas@gmail.com>
2025-01-25 03:59:16 +0900

    250125

---
+++ src/Leetcode/Q3300.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class Q3300 {
+    class Solution {
+        public int getSum(int num) {
+            String snum = String.valueOf(num);
+            int total = 0;
+            for (int i=0; i<snum.length(); i++) {
+                total += Integer.parseInt(snum.charAt(i)+"");
+            }
+            return total;
+        }
+        public int minElement(int[] nums) {
+            int ans = Integer.MAX_VALUE;
+            for (int num : nums) {
+                ans = Math.min(ans, getSum(num));
+            }
+            return ans;
+        }
+    }
+}
