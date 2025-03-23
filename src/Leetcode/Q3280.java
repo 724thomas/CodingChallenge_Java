0892fddcebeece8e1f8df47c0409645b2b2290b3
724thomas <724thomas@gmail.com>
2025-03-23 20:14:12 +0900

    250323

---
+++ src/Leetcode/Q3280.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q3280 {
+    class Solution {
+        public String convertDateToBinary(String date) {
+            StringBuilder sb = new StringBuilder();
+            String[] d = date.split("-");
+            sb.append(Integer.toBinaryString(Integer.parseInt(d[0]))).append("-");
+            sb.append(Integer.toBinaryString(Integer.parseInt(d[1]))).append("-");
+            sb.append(Integer.toBinaryString(Integer.parseInt(d[2])));
+            return sb.toString();
+        }
+    }
+}
