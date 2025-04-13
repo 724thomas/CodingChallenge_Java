913e66b3b1b517a42500e559ab4d7dca8f9466d5
724thomas <724thomas@gmail.com>
2025-04-13 22:00:01 +0900

    250413

---
+++ src/Leetcode/Q2190.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q2190 {
+    class Solution {
+        public int mostFrequent(int[] nums, int key) {
+            int[] counter = new int[1001];
+            int ans = 0, cmax = 0;
+            for (int i=0; i<nums.length-1; i++) {
+                if (nums[i] != key) continue;
+
+                int nextNum = nums[i+1];
+                counter[nextNum]++;
+                int count = counter[nextNum];
+
+                if (count < cmax) continue;
+
+                ans = nextNum;
+                cmax = count;
+            }
+
+            return ans;
+        }
+    }
+}
