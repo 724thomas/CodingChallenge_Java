3904815d2f4ba9f6d4835712de0f2978b5c9eacd
724thomas <724thomas@gmail.com>
2025-04-14 01:41:33 +0900

    250413

---
+++ src/Leetcode/Q1576.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q1576 {
+    class Solution {
+        public Character getChar(char left, char right) {
+            System.out.println(left + " " + right);
+            for (int i=0; i<26; i++) {
+                char mid = (char)(i+'a');
+                if (mid != left && mid != right) return mid;
+            }
+            return 'a';
+        }
+
+        public String modifyString(String s) {
+            int n = s.length();
+            char[] chars = new char[n];
+            for (int i=0; i<n; i++) chars[i] = s.charAt(i);
+
+            for (int i=0; i<n; i++) {
+                if (chars[i] != '?') continue;
+                char left = chars[(n + i-1)%n];
+                char right = chars[(n+i+1)%n];
+                chars[i] = getChar(left, right);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (char c : chars) sb.append(c);
+            return sb.toString();
+        }
+    }
+}
