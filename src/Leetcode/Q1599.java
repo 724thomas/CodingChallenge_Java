0416b9f7bf6b286c2847a17ea57ccb5a17b2b7be
724thomas <724thomas@gmail.com>
2025-04-23 07:00:47 +0900

    250423

---
+++ src/Leetcode/Q1599.java
@@ -0,0 +1,44 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q1599 {
+    class Solution {
+        public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
+            int waiting = 0, profit = 0;
+            int maxProfit = 0, ans = -1;
+            int customersIdx = 0, round = 0;
+
+            while (waiting > 0 || customersIdx < customers.length) {
+                round++;
+
+                if (customersIdx < customers.length) {
+                    waiting += customers[customersIdx++];
+                }
+
+                int board = Math.min(4, waiting);
+                waiting -= board;
+
+                profit += board * boardingCost - runningCost;
+
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                    ans = round;
+                }
+                System.out.println(profit);
+            }
+
+            return ans;
+        }
+    }
+
+}
