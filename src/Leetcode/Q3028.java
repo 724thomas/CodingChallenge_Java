b143f8ede9b7b2fa543198303b2a155de624045c
724thomas <724thomas@gmail.com>
2025-04-26 04:38:12 +0900

    250426

---
+++ src/Leetcode/Q3028.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q3028 {
+    class Solution {
+        public int returnToBoundaryCount(int[] nums) {
+            int curr = 0, ans = 0;
+            for (int num : nums) {
+                curr += num;
+                if (curr == 0) ans++;
+            }
+            return ans;
+        }
+    }
+}
