6a6dada88886f8a942a67035a5575234c36aad58
724thomas <724thomas@gmail.com>
2025-05-10 22:05:31 +0900

    250510

---
+++ src/Leetcode/Q3263.java
@@ -0,0 +1,40 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q3263 {
+/*
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+};
+
+
+    class Solution {
+        public int[] toArray(Node head) {
+            List<Integer> nums = new ArrayList<>();
+            while (head!=null) {
+                nums.add(head.val);
+                head = head.next;
+            }
+            int[] ans = new int[nums.size()];
+            for (int i=0; i<nums.size(); i++) {
+                ans[i] = nums.get(i);
+            }
+            return ans;
+        }
+    }
+ */
+}
