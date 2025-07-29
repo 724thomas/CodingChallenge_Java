458e3a6dd44d6f40790dd0bd690ab820974168d8
724thomas <724thomas@gmail.com>
2025-07-29 22:27:37 +0900

    250729

--- src/백준/Main.java
+++ src/백준/Main.java
@@ -2,13 +2,13 @@
 package 백준;
 
 /*
-
 1. 아이디어 :
 
-2. 시간복잡도 :
-    O( )
-3. 자료구조 :
 
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
 
  */
 import java.io.BufferedReader;
@@ -22,11 +22,14 @@ class Main {
     public static void main(String[] args) throws IOException {
 
 
-//        input = br.readLine();
-//        inputs = br.readLine().split(" ");
-//        ninputs = Arrays.stream(br.readLine().split(" "))
-//                .mapToInt(Integer::parseInt)
-//                .toArray();
+        String input = br.readLine();
+        String[] inputs = br.readLine().split(" ");
+        int[] ninputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int n = Integer.parseInt(br.readLine());
+        char[] chars = br.readLine().toCharArray();
+
 
     }
 }
