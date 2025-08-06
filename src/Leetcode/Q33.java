7711f8577bdab1ef32623a2cbc6c201451b94a1a
724thomas <724thomas@gmail.com>
2025-08-06 17:30:29 +0900

    250806

---
+++ src/Leetcode/Q33.java
@@ -0,0 +1,44 @@
+package Leetcode;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q33 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0, right = nums.length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) return mid;
+
+                // 왼쪽이 정렬된 경우
+                if (nums[left] <= nums[mid]) {
+                    if (nums[left] <= target && target < nums[mid]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] < target && target <= nums[right]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+
+}
