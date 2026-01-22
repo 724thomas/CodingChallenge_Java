ee7135b2bdcb0bcb81b801d4071eac9145dff569
724thomas <724thomas@gmail.com>
2026-01-23 01:59:37 +0900

    Q110

---
+++ src/Leetcode/java/Q110.java
@@ -0,0 +1,36 @@
+package Leetcode.java;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q110 {
+/**
+ * class Solution {
+ *     public boolean isBalanced(TreeNode root) {
+ *         return height(root) != -1;
+ *     }
+ *
+ *     private int height(TreeNode node) {
+ *         if (node == null) return 0;
+ *
+ *         int left = height(node.left);
+ *         if (left == -1) return -1;
+ *
+ *         int right = height(node.right);
+ *         if (right == -1) return -1;
+ *
+ *         if (Math.abs(left - right) > 1) return -1;
+ *
+ *         return Math.max(left, right) + 1;
+ *     }
+ * }
+ */
+}
