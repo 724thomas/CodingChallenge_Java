7c5a1d6e6399d640da0a9b706877fd7b3f6a98d0
724thomas <724thomas@gmail.com>
2026-02-25 10:15:09 +0900

    Q141

---
+++ src/Leetcode/java/Q141.java
@@ -0,0 +1,42 @@
+package Leetcode.java;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q141 {
+//    /**
+//     * Definition for singly-linked list.
+//     * class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode(int x) {
+//     *         val = x;
+//     *         next = null;
+//     *     }
+//     * }
+//     */
+//    public class Solution {
+//        public boolean hasCycle(ListNode head) {
+//            if (head == null || head.next == null || head.next.next == null) return false;
+//            ListNode fast = head.next.next, slow = head.next;
+//
+//            while (true) {
+//                if (fast == slow) return true;
+//
+//                if (fast.next == null || fast.next.next == null) return false;
+//                if (slow.next == null) return false;
+//
+//                fast = fast.next.next;
+//                slow = slow.next;
+//            }
+//        }
+//    }
+}
