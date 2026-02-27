27dcaa816f3cb99286ada63fc745d6eebe0d29a0
724thomas <724thomas@gmail.com>
2026-02-27 23:52:50 +0900

    Q104

---
+++ src/Leetcode/java/Q104.java
@@ -0,0 +1,40 @@
+package Leetcode.java;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q104 {
+//    /**
+//     * Definition for a binary tree node.
+//     * public class TreeNode {
+//     *     int val;
+//     *     TreeNode left;
+//     *     TreeNode right;
+//     *     TreeNode() {}
+//     *     TreeNode(int val) { this.val = val; }
+//     *     TreeNode(int val, TreeNode left, TreeNode right) {
+//     *         this.val = val;
+//     *         this.left = left;
+//     *         this.right = right;
+//     *     }
+//     * }
+//     */
+//    class Solution {
+//        public int maxDepth(TreeNode root) {
+//            return dfs(root, 0);
+//        }
+//
+//        public int dfs(TreeNode node, int cmax) {
+//            if (node == null) return cmax;
+//            return Math.max(dfs(node.left, cmax+1), dfs(node.right, cmax+1));
+//        }
+//    }
+}
