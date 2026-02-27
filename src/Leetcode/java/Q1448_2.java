a72b9626460495b33dd30eee890fe6c2c164bd28
724thomas <724thomas@gmail.com>
2026-02-27 23:53:22 +0900

    Q1448

---
+++ src/Leetcode/java/Q1448_2.java
@@ -0,0 +1,47 @@
+package Leetcode.java;
+
+/*
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+O( )
+
+3. 자료구조/알고리즘 :
+
+ */
+
+public class Q1448_2 {
+//    /**
+//     * Definition for a binary tree node.
+//     * public class TreeNode {
+//     *     int val;
+//     *     TreeNode left;
+//     *     TreeNode right;
+//     *     TreeNode() {}
+//     *     TreeNode(int val) { this.val = val; }
+//     *     TreeNode(int val, TreeNode left, TreeNode right) {
+//     *         this.val = val;
+//     *         this.left = left;
+//     *         this.right = right;
+//     *     }
+//     * }
+//     */
+//    class Solution {
+//        int ans = 0;
+//        public int goodNodes(TreeNode root) {
+//            dfs(root, root.val);
+//            return ans;
+//        }
+//
+//        public void dfs(TreeNode node, int cmax) {
+//            if (node == null) return;
+//            if (node.val >= cmax) ans++;
+//
+//            cmax = Math.max(node.val , cmax);
+//
+//            dfs(node.left, cmax);
+//            dfs(node.right, cmax);
+//        }
+//    }
+}
